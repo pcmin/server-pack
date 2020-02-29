abfc64d32212cca1d44f9cf73865608c931d4702
pcmin <55571684+pcmin@users.noreply.github.com>
2020-02-29 10:48:40 +0900

    add tokenlist template

--- src/settemplate.js
+++ src/settemplate.js
@@ -28,6 +28,21 @@ function uploadProcess(files){
     }
 }
 
+// 토큰 양식 반환함수
+function template(img, name, rent, cnt, pos){return `<img src="${img}" class="thumbnail" width="100%"><div class="textgroup"><div><div class="text name" title="${name}">${name}</div><div class="text rent" title="${rent}">${rent}</div><div class="text count" title="${cnt}">${cnt}</div></div><div class="text pos" title="${pos}">${pos}</div></div><button class="btn delete" title="삭제하기"><svg viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg></button>`;}
+
+// 토큰 추가
+function addToken(data){
+    var tok = document.createElement("div");
+    tok.classList.add("token");
+    tok.innerHTML = template(data.img, data.name, data.rent, data.count, data.pos);
+    document.getElementById("tokenlist").appendChild(tok);
+}
+// 토큰 삭제
+function delelteToken(ev){
+    ev.target.parentElement.removeChild(ev.target);
+}
+
 // 메뉴 클릭
 var menuList = document.querySelectorAll("#menu>li");
 for(let i=0; i<menuList.length; i++){
@@ -38,36 +53,3 @@ for(let i=0; i<menuList.length; i++){
         ev.target.classList.add("menucheck");
     });
 }
-
-const template = [
-`<form action="/save" method="post" id="inputForm">
-    <div id="inputArea">
-        <div id="textUpload">
-            <div class="label">이름</div>
-            <input type="text" class="inputDetail" name="name" placeholder="이름(필수)" required>
-            <div class="label">위치</div>
-            <input type="text" class="inputDetail" name="position" placeholder="위치(GPS 태그 또는 글)">
-            <div class="label">재고/수량</div>
-            <input type="number" class="inputDetail" name="count" placeholder="재고/수량">
-            <div class="label">대여여부</div>
-            <input type="text" class="inputDetail" name="rental" placeholder="대여여부">
-            <div class="label">설명</div>
-            <textarea type="text" class="inputDetail" name="description" placeholder="물건에 대한 구체적인 설명을 입력해주세요"></textarea>
-        </div>
-        <div id="imageUpload">
-            <input id="imageBuffer" type="hidden" name="image">
-            <div id="imageSubmit">
-                <input type="button" value="파일 선택" onclick="imageSelect()">
-                <div id="imageFileName">선택된 파일 없음</div>
-            </div>
-            <div id="imageArea">
-                <img id="previewImage" src="#" alt="이미지 없음" width="auto">
-            </div>
-        </div>
-    </div>
-    <div id="submitArea">
-        <input class="btn" type="submit">
-        <input class="btn" type="reset">
-    </div>
-</form>`
-];
